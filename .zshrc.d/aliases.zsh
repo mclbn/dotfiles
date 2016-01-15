93496fb4e7a678925080c871ca1f35810abddf3f
Marc Lebrun <marc@buffout.org>
2016-01-15 14:34:36 +0100

    Now override some Prezto alias that are not OpenBSD-compatible (ls, grep)

--- .zshrc.d/aliases.zsh
+++ .zshrc.d/aliases.zsh
@@ -1,5 +1,8 @@
+# overriding previous aliases
+alias ls='ls'
+alias grep='grep'
+
 alias setenv="export"
-# alias ls='ls --color'
 alias ll='ls -l'
 alias l='ll'
 alias c='cd'
@@ -8,8 +11,7 @@ alias objdump='objdump -M intel -D'
 alias gdb='gdb -q'
 alias mp='mkdir -p'
 alias ssh='ssh -o ServerAliveInterval=60 -o ServerAliveCountMax=10 -o UseRoaming=no'
-#alias grep='grep --color=always'
-alias grep_urls="grep -aiRPoH --color=never 'https?://[a-zA-Z0-9\-\_\.\~\!\*'\''\(\)\;\:\@\&\=\+\$\,\/\?\#\[\]\%]+'|sed 's/:/,/'"
+alias grep_urls="grep -aiRPoH 'https?://[a-zA-Z0-9\-\_\.\~\!\*'\''\(\)\;\:\@\&\=\+\$\,\/\?\#\[\]\%]+'|sed 's/:/,/'"
 alias grep_full='grep -aiRPoH'
 alias wget_no_cert_verify 'wget --no-check-certificate'5D
 alias tree='tree | less'
