b23cd2fea8a5a3523a977e0287c1978bc01d3276
Marc Lebrun <mclbn@users.noreply.github.com>
2023-03-03 07:16:29 +0100

    new zsh variables for colored output

--- .zshrc.d/variables.zsh
+++ .zshrc.d/variables.zsh
@@ -1,3 +1,5 @@
 export PAGER='less'
 export VISUAL="emacsclient -a "" -t"
 export EDITOR="emacsclient -a "" -t"
+export LESS='-R --use-color -Dd+r$Du+b'
+export MANPAGER="less -R --use-color -Dd+r -Du+b"
