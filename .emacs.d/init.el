e331a3b6d9372e30c1a76f247fd2d5ef8245dc25
Marc Lebrun <mclbn@users.noreply.github.com>
2025-06-24 17:30:34 +0200

    Init.el show off-screen matching parentheses

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,7 +12,6 @@
 ;;   (auto-package-update-hide-results t)
 ;;   :config
 ;;   (auto-package-update-maybe))
-;; show parentheses out of reach (https://with-emacs.com/posts/ui-hacks/show-matching-lines-when-parentheses-go-off-screen/)
 ;; Maybe something to automatically add headers
 ;; Bindings to learn :
 ;; - treemacs
@@ -761,6 +760,53 @@ This is the first function that I (Mehrad) wrote in elisp, so it may still needs
 ;; Show matching parenthesis
 (show-paren-mode 1)
 (setq show-paren-delay 0)
+
+;; Simple hack to display line
+;; when matching parentheses ar off-screen
+;; from https://web.archive.org/web/20201107235946/https://with-emacs.com/posts/ui-hacks/show-matching-lines-when-parentheses-go-off-screen/
+(defun display-line-overlay+ (pos str &optional face)
+  "Display line at POS as STR with FACE.
+
+FACE defaults to inheriting from default and highlight."
+  (let ((ol (save-excursion
+              (goto-char pos)
+              (make-overlay (line-beginning-position)
+                            (line-end-position)))))
+    (overlay-put ol 'display str)
+    (overlay-put ol 'face
+                 (or face '(:inherit default :inherit highlight)))
+    ol))
+(let ((ov nil)) ; keep track of the overlay
+  (advice-add
+   #'show-paren-function
+   :after
+    (defun show-paren--off-screen+ (&rest _args)
+      "Display matching line for off-screen paren."
+      (when (overlayp ov)
+        (delete-overlay ov))
+      ;; check if it's appropriate to show match info,
+      ;; see `blink-paren-post-self-insert-function'
+      (when (and (overlay-buffer show-paren--overlay)
+                 (not (or cursor-in-echo-area
+                          executing-kbd-macro
+                          noninteractive
+                          (minibufferp)
+                          this-command))
+                 (and (not (bobp))
+                      (memq (char-syntax (char-before)) '(?\) ?\$)))
+                 (= 1 (logand 1 (- (point)
+                                   (save-excursion
+                                     (forward-char -1)
+                                     (skip-syntax-backward "/\\")
+                                     (point))))))
+        ;; rebind `minibuffer-message' called by
+        ;; `blink-matching-open' to handle the overlay display
+        (cl-letf (((symbol-function #'minibuffer-message)
+                   (lambda (msg &rest args)
+                     (let ((msg (apply #'format-message msg args)))
+                       (setq ov (display-line-overlay+
+                                 (window-start) msg ))))))
+          (blink-matching-open))))))
 
 ;; make characters after column 80 purple
 (use-package whitespace
