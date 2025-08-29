dbfc5e0495e445be800ef935ef672670e356bfb0
Marc Lebrun <mclbn@users.noreply.github.com>
2025-08-29 17:30:16 +0200

    Init.el dired keybinds

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -241,6 +241,7 @@
     (let* ((file (dired-get-filename nil t)))
       (call-process "xdg-open" nil 0 nil file)))
   (define-key dired-mode-map (kbd "C-<return>") #'dired-open-external)
+  (define-key dired-mode-map (kbd ";") #'dired-hide-details-mode)
   ;;   (defun open-in-external-app ()
   ;;     "Open the file where point is or the marked files in Dired in external
   ;; app. The app is chosen from your OS's preference."
@@ -259,7 +260,7 @@
   (use-package dired-git-info
     :custom
     (dgi-auto-hide-details-p nil)
-    :bind (:map dired-mode-map (")" . dired-git-info-mode))
+    :bind (:map dired-mode-map (":" . dired-git-info-mode))
     )
 
   :hook
