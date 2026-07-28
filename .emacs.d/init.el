4c705825e90431142c98394517f7202502fefac7
Marc Lebrun <marc@pentest>
2026-07-28 15:47:42 +0200

    Init.el new mcp sources.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3359,6 +3359,8 @@ This is a modified version of `mu4e-view-save-attachments'."
   :custom
   (mcp-hub-servers
    `(("searxng" . (:url ,perso/mcp-searxng-url :timeout 30))
+     ("wikipedia" . (:url ,perso/mcp-wikipedia-url :timeout 30))
+     ("datagouv" . (:url ,perso/mcp-datagouv-url :timeout 30))
      ("jellyfin" . (:url ,perso/mcp-jellyfin-url :timeout 30))
      ("omdb"     . (:url ,perso/mcp-omdb-url     :timeout 30))
      ("tmdb"     . (:url ,perso/mcp-tmdb-url     :timeout 30)))))
